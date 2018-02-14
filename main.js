d332519898b8262a9acceb2db834d82811675437
Li Zhang <bylizhang@gmail.com>
2018-02-14 12:52:01 -0800

    basic operations work now

--- main.js
+++ main.js
@@ -13,33 +13,72 @@ function addClickHandlers() {
 }
 
 function handleNumberClick() {
-	// console.log('number clicked');
 	var numberClicked = $(this).text();
-	dataStorage.push(numberClicked);
-	updateDisplay(dataStorage);
+	if (numberClicked === '=') {
+		var result = doMath(dataStorage);
+		displayResult(result);
+	} else {
+		dataStorage.push(numberClicked);
+		updateDisplay(dataStorage);
+	}
 }
 
 function handleOperatorClick() {
-	// console.log('operator clicked');
 	var operatorClicked = $(this).text();
-	if (dataStorage.length )
-	dataStorage += operatorClicked;
+	if (dataStorage.length) {
+		dataStorage.push(operatorClicked);
+	}
 
 	updateDisplay(dataStorage);
 }
 
 function handleClearClick() {
-	// console.log('clear clicked');
 	if ($(this).text() === 'C') {
 		dataStorage.pop();
-		updateDisplay(dataStorage);
-	} else if ($(this).text() === 'CE') {
+	} 
+	if ($(this).text() === 'CE') {
 		dataStorage = [];
-		updateDisplay(dataStorage);
 	}
+	updateDisplay(dataStorage);
 }
 
 function updateDisplay(inputData){
-	// $('.screen').text('');
+	var message = inputData.join(' ');
+	$('.screen').text(message);
+}
+
+function displayResult(inputData) {
 	$('.screen').text(inputData);
+	dataStorage = [];
+}
+
+function doMath(inputData) {
+	var num1 = '';
+	var num2 = '';
+	var firstOperatorSeen = false;
+	var secondOperatorSeen = false;
+	var operators = ['+', '-', '*', '/'];
+	for (var i = 0; i < inputData.length; i++){
+		if (operators.includes(inputData[i]) && !firstOperatorSeen){
+			firstOperatorSeen = inputData[i]; 
+		} else if (firstOperatorSeen && !secondOperatorSeen) {
+			num2 += inputData[i];
+		} else if (operators.includes(inputData[i]) && firstOperatorSeen) {
+			secondOperatorPostion = i;
+		}
+		else {
+			num1 += inputData[i];
+		}
+	}
+	if (firstOperatorSeen === '+') {
+		return Number(num1) + Number(num2);
+	} else if (firstOperatorSeen === '-') {
+		return Number(num1) - Number(num2);
+	} else if (firstOperatorSeen === '*') {
+		return Number(num1) * Number(num2);
+	} else if (firstOperatorSeen === '/' && num2 != 0) {
+		return Number(num1)/Number(num2);
+	} else {
+		return 'Error';
+	}
 }
